4c7277ce694de8a7d5ea8a494a2ba41304622005
user <chump7@naver.com>
2020-08-16 07:57:30 +0900

    Commit

---
+++ src/main/java/com/example/demo/CharacterView.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class CharacterView {
+    public void printView(CharacterModel model) { System.out.println("Name :: " + model.getName()); System.out.println("Level :: " + model.getLevel()); System.out.println("Life :: " + model.getLife()); }
+
+
+}
--- src/main/java/com/example/demo/SimpleGameApplication.java
+++ src/main/java/com/example/demo/SimpleGameApplication.java
@@ -6,29 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleGameApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { CharacterModel model = new CharacterModel("Crocus", 20, 3); CharacterView view = new CharacterView(); CharacterController controller = new CharacterController(model, view); controller.updateView(); controller.setCharacterLevel(controller.getCharacterLevel() + 1); controller.updateView(); controller.setCharacterLife(controller.getCharacterLife() + 10); controller.updateView(); }
 
-		//fetch student record based on his roll no from the database
-		Student model  = retriveStudentFromDatabase();
-
-		//Create a view : to write student details on console
-		StudentView view = new StudentView();
-
-		StudentController controller = new StudentController(model, view);
-
-		controller.updateView();
-
-		//update model data
-		controller.setStudentName("John");
-
-		controller.updateView();
-	}
-
-	private static Student retriveStudentFromDatabase(){
-		Student student = new Student();
-		student.setName("Robert");
-		student.setRollNo("10");
-		return student;
-	}
 
 }
